7a22f2859b39856bbaccd297554882915ab6f61b
PrzemyDev <101277573+PrzemyDev@users.noreply.github.com>
2022-08-08 19:24:08 +0200

    EncryptionApp v1.0
    Added new Android Application for encrypting and decrypting messages!

---
+++ EncryptionApp/EncryptionApp/AES.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptionApp
+{
+    class AES
+    {
+        public AES()
+        {
+
+        }
+
+        //Encrypt string to bytes
+        public byte[] Encryption(string plainText, byte[] Key, byte[] IV)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            byte[] encryptedInside;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encryptedInside = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return encryptedInside;
+        }
+
+        //Decrypt string from bytes
+        public string Decription(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentException("IV");
+
+            string plaintext = null;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecryptor = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecryptor, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plaintext;
+        }
+
+    }
+}
---
+++ EncryptionApp/EncryptionApp/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace EncryptionApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "EncApp");
+        AES aes = new AES();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //temp 1234567890123456
+        private string KeyInput;
+        private string IV = "żź05sy!y3Sa9HH*@";
+        private bool IsKeyCorrect = false;
+
+        private async void btnAcceptKey_Clicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(entKey.Text) || entKey.Text.Length < 16 || (entKey.Text.Length > 16 && entKey.Text.Length < 32) || entKey.Text.Length > 16 && entKey.Text.Length > 32)
+            {
+                await DisplayAlert("Klucz", "Klucz musi zawierać 16 lub 32 znaków", "OK");
+                IsKeyCorrect = false;
+            }
+            else
+            {
+                KeyInput = entKey.Text;
+                IsKeyCorrect = true;
+                lblKeyInfo.Text = "Klucz poprawny!";
+                await Task.Delay(2000);
+                lblKeyInfo.Text = "EncryptionApp";
+                entKey.IsReadOnly = true;
+            }
+        }
+        private void btnClearKey_Clicked(object sender, EventArgs e) 
+        {
+            entKey.Text = String.Empty;
+            entKey.IsReadOnly = false;
+        }
+
+        private void btnEncrypt_Clicked(object sender, EventArgs e)
+        {
+            if (IsKeyCorrect && edtUserInput.Text != string.Empty)
+            {
+                byte[] bKey = Encoding.ASCII.GetBytes(KeyInput);
+                byte[] bIV = Encoding.ASCII.GetBytes(IV);
+                byte[] bEncrypt = aes.Encryption(edtUserInput.Text, bKey, bIV);
+                edtUserInput.Text = Convert.ToBase64String(bEncrypt);
+            }
+            else
+            {
+                DisplayAlert("Info", "Najpierw należy zatwierdzić klucz oraz podać tekst do zaszyfrowania.", "OK");
+            }
+           
+        }
+        private void btnDecrypt_Clicked(object sender, EventArgs e)
+        {
+            if (IsKeyCorrect && edtUserInput.Text != string.Empty)
+            {
+                byte[] bKey = Encoding.ASCII.GetBytes(KeyInput);
+                byte[] bIV = Encoding.ASCII.GetBytes(IV);
+                byte[] bInput = Convert.FromBase64String(edtUserInput.Text);
+                string sDecrypt = aes.Decription(bInput, bKey, bIV);
+                edtUserInput.Text = sDecrypt;
+            }
+            else
+            {
+                DisplayAlert("Info", "Najpierw należy zatwierdzić klucz oraz podać tekst do odszyfrowania.", "OK");
+            }
+        }
+        private async void btnCopy_Clicked(object sender, EventArgs e)
+        {
+            await Clipboard.SetTextAsync(edtUserInput.Text);
+            lblKeyInfo.Text = "Skopiowano wiadomość do schowka!";
+            await Task.Delay(2000);
+            lblKeyInfo.Text = "EncryptionApp";
+        }
+        private async void btnPaste_Clicked(object sender, EventArgs e)
+        {
+            edtUserInput.Text = await Clipboard.GetTextAsync();
+        }
+        private void btnClear_Clicked(object sender, EventArgs e)
+        {
+            edtUserInput.Text = String.Empty;
+        }
+        private void btnHelp_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Pomoc", "Aby zaszyfrować lub odszyfrować wiadomość należy wpisać klucz i wcisnąć przycisk \u0022ZATWIERDŹ\u0022. Tekst do szyfrowania i odszyfrowania należy podać poniżej. \nOznaczenia przycisków belki od lewej strony: \nZaszyfruj i Odszyfruj wiadomość, Skopiuj wiadomość do schowka, Wklej wiadomość ze schowka, Wyczyść wiadomość, Wyświetl pomoc, Wyłącz aplikację.\n\nAplikacja napisana przez:\nhttps://github.com/PrzemyDev - 2022", "OK");
+        }
+        private void btnExit_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+        
+    }
+}
